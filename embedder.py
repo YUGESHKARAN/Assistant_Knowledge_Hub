a50c9629dea1d6d2ef905e6435b4f51ae7669247
YUGESHKARAN <yugeshkaran01@gmail.com>
2026-05-01 08:57:57 +0530

    temporary update of embedding model from text-embedding-3-small to llama-text-embed-v2

--- embedder.py
+++ embedder.py
@@ -1,13 +1,29 @@
 
-from openai import OpenAI
-from utility.config import EMBED_MODEL
+# from openai import OpenAI
+# from utility.config import EMBED_MODEL
 
-client = OpenAI()
+# client = OpenAI()
+
+# def generate_embedding(text: str):
+#     res = client.embeddings.create(
+#         model=EMBED_MODEL,
+#         input=text,
+#         dimensions=512
+#     )
+#     return res.data[0].embedding
+
+from pinecone import Pinecone
+from utility.config import PINECONE_API_KEY
+pc = Pinecone(api_key=PINECONE_API_KEY)
 
 def generate_embedding(text: str):
-    res = client.embeddings.create(
-        model=EMBED_MODEL,
-        input=text,
-        dimensions=512
+    embeddings = pc.inference.embed(
+        model="llama-text-embed-v2",
+        inputs=[{"text": text}],
+        parameters={
+            "input_type": "passage",
+            "truncate": "END",
+            "dimension": 512
+        }
     )
-    return res.data[0].embedding
+    return embeddings[0]["values"]

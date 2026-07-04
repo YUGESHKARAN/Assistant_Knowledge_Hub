d5487117efa847e71b4450c655736ae29ef82ae1
YUGESHKARAN <yugeshkaran01@gmail.com>
2026-07-04 19:01:40 +0530

    model update reverted from openai/gpt-oss-120b to openai/gpt-oss-20b.

--- utility/config.py
+++ utility/config.py
@@ -12,8 +12,8 @@ EMBED_MODEL = "text-embedding-3-small"
 # LLM_MODEL = "llama-3.1-8b-instant"
 # LLM_MODEL = "meta-llama/llama-4-maverick-17b-128e-instruct"
 # LLM_MODEL = "meta-llama/Llama-4-Scout-17B-16E-Instruct"
-# LLM_MODEL = "openai/gpt-oss-120b"
-LLM_MODEL = "openai/gpt-oss-20b"
+LLM_MODEL = "openai/gpt-oss-120b"
+# LLM_MODEL = "openai/gpt-oss-20b"
 
 
 def build_response(

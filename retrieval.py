d844380f6543af0c0c0569e1fb5e1aeebda03b3e
YUGESHKARAN <yugeshkaran01@gmail.com>
2026-07-12 11:38:21 +0530

    Context description reduced from 1500 to 1000 words.

--- retrieval.py
+++ retrieval.py
@@ -103,7 +103,7 @@ def detect_youtube_links(posts):
     return videos
 
 
-MAX_DESCRIPTION_LENGTH = 1500
+MAX_DESCRIPTION_LENGTH = 1000
 
 def ask_ai(query: str, current_post_id: str, category:str, SYSTEM_PROMPT:str) :
     docs = retrieve(query,current_post_id, category)
